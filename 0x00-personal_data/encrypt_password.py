e04bbb6aaa28a8b0816f9c8665cc8adcd84e517c
Soukaina <megdani20soukaina@gmail.com>
2024-11-22 00:43:15 +0100

    done

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    I use the bcrypt package to perform
+    the hashing (with hashpw).
+    """
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check valid password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
