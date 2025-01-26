bde9f6154871e56119bd6ea3b6cdc93e467ed45f
Soukaina <megdani20soukaina@gmail.com>
2025-01-26 17:02:34 +0100

    user auth

---
+++ 0x03-user_authentication_service/main_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""main_0 module"""
+
+
+from user import User
+
+print(User.__tablename__)
+
+for column in User.__table__.columns:
+    print("{}: {}".format(column, column.type))
