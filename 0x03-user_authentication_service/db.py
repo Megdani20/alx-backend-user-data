e69c0150aaefea8142a1250a69730aa0412b0aff
Soukaina <megdani20soukaina@gmail.com>
2025-01-26 17:14:33 +0100

    find user by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base, User
+from .user import Base, User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
@@ -34,4 +36,13 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+    
+    def find_user_by(self, **args):
+        try:
+            user = self.__session.query(User).filter_by(**args).first()
+        except Exception:
+            raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
         return user
